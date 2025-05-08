d02b7fa92ef59f865c80e51bb2782af6422d841b
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-05-08 18:34:25 +0200

    solution uppg6

--- uppg6.js
+++ uppg6.js
@@ -4,6 +4,11 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+    let names = ["bob", "bill", "mary", "may"]
+
+    for (n of names){
+        console.log(n)
+    }
 
  
 }

263766cba7c7de8ae75818fafa7663632bc4b240
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-05-08 18:31:39 +0200

    solution uppg5

--- uppg5.js
+++ uppg5.js
@@ -3,6 +3,15 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+ 
+    let a = 10
+    let b = 20
+
+    if (a > b){
+        console.log("Det första talet är större än det andra")
+    } else {
+        console.log("Det andra talet är större än det första")
+    }
 
 
 
